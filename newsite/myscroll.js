4485ae81882586737e253187c7e0cb863567b45b
brydon <brydoneastman@gmail.com>
2022-01-10 03:49:14 -0500

    magic numbers

--- newsite/myscroll.js
+++ newsite/myscroll.js
@@ -2,8 +2,10 @@ window.onload = function() {scrollFunction()};
 window.onresize = function() {scrollFunction()};
 window.onscroll = function() {scrollFunction()};
 
+var pix_thresh = 20;
+
 function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    if (document.body.scrollTop > pix_thresh || document.documentElement.scrollTop > pix_thresh) {
         document.getElementById("navbar").classList.remove('nobg');
     } else {
         if (!document.getElementById("navbarNav").classList.contains("show")) {
@@ -14,7 +16,7 @@ function scrollFunction() {
 
 function showBg() {
     document.getElementById("navbar").classList.remove('nobg');
-    if (document.body.scrollTop <= 20 && document.documentElement.scrollTop <= 20) {
+    if (document.body.scrollTop <= pix_thresh && document.documentElement.scrollTop <= pix_thresh) {
         if (document.getElementById("navButton").classList.contains("collapsed")) {
             document.getElementById("navbar").classList.add('nobg');
         } 
